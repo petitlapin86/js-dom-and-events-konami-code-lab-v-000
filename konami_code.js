bc296bb0208619fdda487665e784d3fcb9662bfd
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-21 15:30:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,10 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
 
 }
+
+alphabet.addEventListener('keydown', () =>{
+   myHeading.style.color = myTextInput.value; 
+})
