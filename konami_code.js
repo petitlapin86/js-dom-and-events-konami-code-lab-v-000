ebb55400b864c1520fbcdc8dd50eab27f0e47963
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-21 15:31:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,5 +6,19 @@ function init() {
 }
 
 alphabet.addEventListener('keydown', () =>{
-   myHeading.style.color = myTextInput.value; 
+  function onKeyDownHandler(e) {
+ const key = parseInt(e.detail || e.which);
+
+ if (key === alphabet[index]) {
+   index++;
+
+   if (index === alphabet.length) {
+     alert("Hurray!");
+
+     index = 0;
+   }
+ } else {
+   index = 0;
+ }
+}
 })
